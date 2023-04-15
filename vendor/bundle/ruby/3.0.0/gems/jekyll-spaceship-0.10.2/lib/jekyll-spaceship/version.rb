1e03ddac0cd3f8df34118b32a22eb86aa6b98ccb
Sturla Bragason <sturla.bragason@devoteam.com>
2023-04-15 21:41:03 +0000

    gems

--- vendor/bundle/ruby/3.0.0/gems/jekyll-spaceship-0.10.2/lib/jekyll-spaceship/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Jekyll
-  module Spaceship
-    VERSION = "0.10.2"
-  end
-end
--- vendor/bundle/ruby/3.0.0/gems/sass-embedded-1.62.0/lib/sass/embedded/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Sass
-  class Embedded
-    VERSION = '1.62.0'
-  end
-end
